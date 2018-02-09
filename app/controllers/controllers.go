9dd4b4c923ac175273a34c95b982c2334332d9b8
deepaksworks <kunal.mawar@sworks.co.in>
2018-02-09 13:28:31 +0530

    removing unwanted comments

--- app/controllers/controllers.go
+++ app/controllers/controllers.go
@@ -2,45 +2,14 @@ package controllers
 
 import (
     "fmt"
-    //"time"
     "net/http"
    _"github.com/jinzhu/gorm/dialects/mysql"
     "github.com/labstack/echo"
    	"uktrav_echo/app"
    	"github.com/jinzhu/gorm"
-	//cgorm "uktrav_echo/db/gorm"
     "uktrav_echo/app/models"
     
 )
-
-/*
-var DB *gorm.DB
-
-func initDB(){
-	DB, err := gorm.Open("mysql", "smartworks:smartworks@/uktrav?charset=utf8")
-	if err!=nil {
-            fmt.Println("DB not Connected")       
-     }
-    defer DB.Close()
-    
-}
-
-
-
-type User struct {
-    Id           int       `gorm:"AUTO_INCREMENT;primary_key;column:id"`
-    Fname        string    `gorm:"size:30;column:fname"`
-    Lname        string    `gorm:"size:30;column:lname"`
-    Email        string    `gorm:"size:30;column:email"`
-    PasswordSalt string    `gorm:"size:30;column:password_salt"`
-    Authcode     string    `gorm:"size:30;column:authcode"`
-    Phone        string    `gorm:"size:15;column:phone"`
-    CreatedDt    time.Time `gorm:"column:created_dt"`
-    Status       int       `gorm:"column:status"`
-}
-
-*/
-
 
 type H map[string]interface{}
 
@@ -49,9 +18,7 @@ func Init() {
       DB, err := gorm.Open("mysql", "smartworks:smartworks@/uktrav?charset=utf8&parseTime=True")
 	  if err!=nil {
             fmt.Println("DB not Connected")       
-          }
-      //defer DB.Close()
-      
+          }   
       app.Server.GET("/users",get_last_user(DB))
 }
 
@@ -60,12 +27,11 @@ func get_last_user(db *gorm.DB) echo.HandlerFunc {
 
     return func(c echo.Context) error {
             var user models.User
-            //obj := DB.First(&user,1)
-            //fmt.Println(DB.HasTable(&User{}))
             db.Last(&user)
             fmt.Println(user.Fname)
-            //defer DB.Close()
-	        return c.JSON(http.StatusCreated, H{"id" : user.Id})
+	        return c.JSON(http.StatusCreated, 
+	        H{"id" : user.Id,
+	        })
       }
 
 }
--- app/models/models.go
+++ app/models/models.go
@@ -1,63 +1,19 @@
 package models
 
 import (
-       //"github.com/jinzhu/gorm"
        "time"
 )
-
-/*
-    desc user;
-+---------------+-------------+------+-----+---------+----------------+
-| Field         | Type        | Null | Key | Default | Extra          |
-+---------------+-------------+------+-----+---------+----------------+
-| id            | int(11)     | NO   | PRI | NULL    | auto_increment |
-| fname         | varchar(30) | NO   |     | NULL    |                |
-| lname         | varchar(30) | YES  |     | NULL    |                |
-| email         | varchar(30) | YES  |     | NULL    |                |
-| password_salt | varchar(30) | NO   |     | NULL    |                |
-| authcode      | varchar(30) | YES  |     | NULL    |                |
-| phone         | varchar(15) | NO   |     | NULL    |                |
-| created_dt    | datetime    | NO   |     | NULL    |                |
-| status        | tinyint(1)  | YES  |     | 0       |                |
-+---------------+-------------+------+-----+---------+----------------+
-*/
 
 type User struct {
     Id           int       `gorm:"AUTO_INCREMENT;primary_key;column:id"`
     Fname        string    `gorm:"size:30;column:fname"`
     Lname        string    `gorm:"size:30;column:lname"`
     Email        string    `gorm:"size:30;column:email"`
-    PasswordSalt string    `gorm:"size:30;column:password_salt"`
+    Password     string    `gorm:"size:30;column:password_salt"`
     Authcode     string    `gorm:"size:30;column:authcode"`
     Phone        string    `gorm:"size:15;column:phone"`
     CreatedDt    time.Time `gorm:"column:created_dt"`
     Status       int       `gorm:"column:status"`
 }
-/*
-get_users := func(c echo.Context) error {
-             var user []User
-             res := getGormDB().Find(&user)
-             fmt.Println(res)
-	         return c.String(http.StatusOK, "OK")
-}
 
-
-    Birthday     time.Time
-    Age          int
-    Name         string  `gorm:"size:255"` // Default size for string is 255, reset it with this tag
-    
-
-    CreditCard        CreditCard      // One-To-One relationship (has one - use CreditCard's UserID as foreign key)
-    Emails            []Email         // One-To-Many relationship (has many - use Email's UserID as foreign key)
-
-    BillingAddress    Address         // One-To-One relationship (belongs to - use BillingAddressID as foreign key)
-    BillingAddressID  sql.NullInt64
-
-    ShippingAddress   Address         // One-To-One relationship (belongs to - use ShippingAddressID as foreign key)
-    ShippingAddressID int
-
-    IgnoreMe          int `gorm:"-"`   // Ignore this field
-    Languages         []Language `gorm:"many2many:user_languages;"` // Many-To-Many relationship, 'user_languages' is join table
-    
- */
 
